a8a38e0bc408a33c9177b98a5ee034b063c8674f
JvTadeo <joao.tadeot2@gmail.com>
2022-11-12 23:19:45 -0300

    Botões arrumados

--- windowns.py
+++ windowns.py
@@ -3,31 +3,36 @@ from tkinter import *
 root = Tk()
 root.title("SISTEMA DE ESTACIONAMENTO")
 root.geometry("640x520")
+root.resizable(False, False)
+
+car = PhotoImage(file="Imagens\Car.png") 
 
 
 class MainWindows(Tk):
     def __init__(self, root):
 
+        #DECLANDO BOTÕES
         self.nameSystem = Label(root, text="GERENCIAMENTO DO ESTACIONAMENTO", font=('Roboto', 15), fg = 'white', bg='#CF0A0A', width=100, height=2, anchor=W, padx = 10)
 
-        self.vaga1 = Button(bg="#00C130", width=9, height=8, relief = GROOVE)
-        self.vaga2 = Button(bg="#00C130", width=9, height=8, relief = GROOVE)
-        self.vaga3 = Button(bg="#00C130", width=9, height=8, relief = GROOVE)
-        self.vaga4 = Button(bg="#00C130", width=9, height=8, relief = GROOVE)
-        self.vaga5 = Button(bg="#00C130", width=9, height=8, relief = GROOVE)
-        self.vaga6 = Button(bg="#00C130", width=9, height=8, relief = GROOVE)
+        self.vaga1 = Button(bg="#00C130", width=9, height=8, relief = GROOVE, bd = -1)
+        self.vaga2 = Button(bg="#00C130", width=9, height=8, relief = GROOVE, bd = -1)
+        self.vaga3 = Button(bg="#00C130", width=9, height=8, relief = GROOVE, bd = -1)
+        self.vaga4 = Button(bg="#00C130", width=9, height=8, relief = GROOVE, bd = -1)
+        self.vaga5 = Button(bg="#00C130", width=9, height=8, relief = GROOVE, bd = -1)
+        self.vaga6 = Button(bg="#00C130", width=9, height=8, relief = GROOVE, bd = -1)
 
+        self.nameSystem.grid(column=1, row=0, columnspan = 30)
 
-        self.nameSystem.grid(column=1, row=0, columnspan = 10)
-        self.vaga1.grid(column=1, row=1, sticky=W, padx=20, pady=50)
-        self.vaga2.grid(column=2, row=1, sticky='')
-        self.vaga3.grid(column=3, row=1, sticky=E)
-        self.vaga4.grid(column=1, row=2, sticky=W, padx=20, pady=50)
-        self.vaga5.grid(column=2, row=2, sticky='')
-        self.vaga6.grid(column=3, row=2, sticky=E)
+        #COLOCANDO OS BOTÕES
+        self.vaga1.place(x=130, y=120)
+        self.vaga2.place(x=280, y=120)
+        self.vaga3.place(x=430, y=120)
+
+        self.vaga4.place(x=130, y=290)
+        self.vaga5.place(x=280, y=290)
+        self.vaga6.place(x=430, y=290)         
+
         
-            
-
         
         
 
