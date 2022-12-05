186452a4d0266f91d2ed37b1d38b171aece378fe
JvTadeo <joao.tadeot2@gmail.com>
2022-12-05 15:15:56 -0300

    Atualização sobre o Graph

--- graph.py
+++ graph.py
@@ -1,5 +1,3 @@
-from gettext import npgettext
-import matplotlib as mpl
 import matplotlib.pyplot as plt
 import pandas
 import sqlite3 as sql
@@ -11,10 +9,9 @@ data = pandas.read_sql(selectAll, con)
 
 
 
-# Note that even in the OO-style, we use `.pyplot.figure` to create the Figure.
 fig, ax = plt.subplots(figsize=(5, 2.7), layout='constrained')
-ax.plot(data.HORA_PAGA,data.MODELO)  # Plot some data on the axes.
-ax.set_xlabel('HORAS PAGAS')  # Add an x-label to the axes.
+ax.plot(data.MODELO,data.HORA_PAGA)
+ax.set_xlabel('HORAS PAGAS') 
 ax.set_ylabel('MODELOS')  # Add a y-label to the axes.
 ax.set_title("GRÁFICO DO ESTACIONAMENTO")  # Add a title to the axes.
 plt.show()
